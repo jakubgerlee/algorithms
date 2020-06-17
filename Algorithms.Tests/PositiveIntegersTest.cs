4f7258a910a72a51758f2da94dfa56cc1b506c65
Jakub Gerlée <jakubgerlee@gmail.com>
2020-06-18 01:30:03 +0200

    Finish algorithm adding prime number check.

--- Algorithms.Tests/PositiveIntegersTest.cs
+++ Algorithms.Tests/PositiveIntegersTest.cs
@@ -16,14 +16,45 @@ namespace Algorithms.Tests
 		}
 
 		[Test]
-		public void GetTwoPositiveIntegersWhichSumIsEqualToInput_PositiveNumberInput_SumEqualsToInput()
+		public void GetTwoPositiveIntegersWhichSumIsEqualToInput_NumberDividedBy10_SumEqualsToInput()
 		{
-			var expectedResult = 100;
+			var input = 100;
+			var expectedResult = input;
 
-			var result = _positiveIntegers.GetTwoPositiveIntegersWhichSumIsEqualToInput(100);
+			var result = _positiveIntegers.GetTwoPositiveIntegersWhichSumIsEqualToInput(input);
+			var actualResult = result.Sum();
+			
+			Assert.That(actualResult, Is.EqualTo(expectedResult));
+			Assert.That(result[0]%10, Is.Not.Zero);
+			Assert.That(result[1]%10, Is.Not.Zero);
+		}
+
+		[Test]
+		public void GetTwoPositiveIntegersWhichSumIsEqualToInput_NumberCannotBeDivideBy10_SumEqualsToInput()
+		{
+			var input = 8;
+			var expectedResult = input;
+
+			var result = _positiveIntegers.GetTwoPositiveIntegersWhichSumIsEqualToInput(input);
 			var actualResult = result.Sum();
 
 			Assert.That(actualResult, Is.EqualTo(expectedResult));
+			Assert.That(result[0] % 10, Is.Not.Zero);
+			Assert.That(result[1] % 10, Is.Not.Zero);
+		}
+
+		[Test]
+		public void GetTwoPositiveIntegersWhichSumIsEqualToInput_PrimeNumberIsAnInput_SumEqualsToInput()
+		{
+			var input = 7;
+			var expectedResult = input;
+
+			var result = _positiveIntegers.GetTwoPositiveIntegersWhichSumIsEqualToInput(input);
+			var actualResult = result.Sum();
+
+			Assert.That(actualResult, Is.EqualTo(expectedResult));
+			Assert.That(result[0] % 10, Is.Not.Zero);
+			Assert.That(result[1] % 10, Is.Not.Zero);
 		}
 	}
 }
