e6ca8f537e5e066272fd12773902db8e769d3c70
Jakub Gerlée <jakubgerlee@gmail.com>
2020-06-18 00:50:00 +0200

    Created test for new algorithms which finds two integers which sum is equal to input. But each of integer doesnt have zero.

---
+++ Algorithms.Tests/PositiveIntegersTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+	public class PositiveIntegersTest
+	{
+		// D + B = N
+		// D or B cannot contain 0 
+		private PositiveIntegers _positiveIntegers;
+
+		[SetUp]
+		public void Setup()
+		{
+			_positiveIntegers = new PositiveIntegers();
+		}
+
+		[Test]
+		public void GetTwoPositiveIntegersWhichSumIsEqualToInput_PositiveNumberInput_SumEqualsToInput()
+		{
+			var expectedResult = 100;
+
+			var result = _positiveIntegers.GetTwoPositiveIntegersWhichSumIsEqualToInput(100);
+			var actualResult = result.Sum();
+
+			Assert.That(actualResult, Is.EqualTo(expectedResult));
+		}
+	}
+}
