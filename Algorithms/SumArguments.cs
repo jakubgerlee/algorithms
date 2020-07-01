948a2e47dda924f394128738df14cb4003541eda
jgerlee <jakubgerlee@gmail.com>
2020-07-01 15:03:31 +0200

    Added SumArguments.cs

---
+++ Algorithms/SumArguments.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace Algorithms
+{
+	public class SumArguments
+	{
+		//Given an array of ints, write a C# method to total all the values that are even numbers.
+		public long SumEvenNumberFromArray(int[] array) => array.Where(x => x % 2 == 0).Sum(i => i);
+	}
+}
---
+++ TestToDelete/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestToDelete
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var devices =  AvgRotorSpeed("RUNNING", 2);
+			Console.ReadKey();
+		}
+
+		public static async Task<int> AvgRotorSpeed(string statusQuery, int parentId)
+		{
+			var operatingParams = await GetOperatingParams(statusQuery, parentId);
+			var array = operatingParams as int[] ?? operatingParams.ToArray();
+			var sum = array.Sum();
+			var countOfRotorSpeed = array.Count();
+			return sum / countOfRotorSpeed;
+
+		}
+
+		public static async Task<IEnumerable<int>> GetOperatingParams(string statusQuery, int parentId)
+		{
+			var iotDevices = await GetRotorSpeedsFromResource(statusQuery, parentId);
+			return iotDevices.Devices.Select(x => x.OperatingParams.RotorSpeed);
+
+		}
+
+		private static async Task<IotDevices> GetRotorSpeedsFromResource(string statusQuery, int parentId)
+		{
+			try
+			{
+				using var httpClient = new HttpClient();
+				using var response = await httpClient.GetAsync($"https://jsonmock.hackerrank.com/api/iot_devices?status={statusQuery}&page={parentId}");
+				var apiResponse = await response.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<IotDevices>(apiResponse);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+		}
+	}
+}
+
+class IotDevices
+{
+
+	[JsonProperty(PropertyName = "data")]
+	public List<Device> Devices { get; set; }
+
+}
+
+public class Device
+{
+
+	[JsonPropertyName("operatingParams")]
+	public OperatingParams OperatingParams { get; set; }
+
+}
+
+public class OperatingParams
+{
+	public int RotorSpeed { get; set; }
+}
